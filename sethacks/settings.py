12e901f69a6f933d11c94362bbdace927c9f7c12
Bob661161616 <77864522+Bob661161616@users.noreply.github.com>
2021-08-15 10:15:47 -0500

    Update settings.py

--- sethacks/settings.py
+++ sethacks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rbbsxijx+&vpx7t1ze^z$csr8c_+2e-*!4f^%+k=3p!z@*tknu'
+SECRET_KEY = 'REDACTED'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
